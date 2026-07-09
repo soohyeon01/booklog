2b39a45f4d00681d5057e3257db5a37b2dfcfe2d
soohyeon01 <ssuu0216@gmail>
2026-07-09 14:54:49 +0900

    feat: 새 책 등록 기능 구현 (ModelAttribute & PRG 패턴 적용)

--- src/main/java/com/soohyeon/booklog/web/BookController.java
+++ src/main/java/com/soohyeon/booklog/web/BookController.java
@@ -8,7 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -26,11 +29,34 @@ public class BookController {
         return "books/books";
     }
 
+    /**
+     * 새 책 등록 폼 화면 이동
+     * URL: GET /books/add
+     */
+    @GetMapping("/add")
+    public String addForm() {
+        return "books/addForm";
+    }
+
+    /**
+     * 새 책 등록
+     * @ModelAttribute  사용 시, model.addAttribute() 자동 추가
+     * PRG 패턴 적용
+     * URL: POST /books/add
+     */
+    @PostMapping("/add")
+    public String add(@ModelAttribute("book") Book book, RedirectAttributes redirectAttributes) {
+        Book savedBook = bookService.saveBook(book);
+        redirectAttributes.addAttribute("bookId", savedBook.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/books/{bookId}";
+    }
+
     @PostConstruct
     public void init() {
-        bookService.registerBook(new Book("데미안", "헤르만 헤세", BookStatus.READING, 5,
+        bookService.saveBook(new Book("데미안", "헤르만 헤세", BookStatus.READING, 5,
                 "새는 알에서 나오려고 투쟁한다.", "내면의 성장을 유도하는 최고의 책"));
-        bookService.registerBook(new Book("스프링 MVC 1편", "김영한", BookStatus.DONE, 3,
+        bookService.saveBook(new Book("스프링 MVC 1편", "김영한", BookStatus.DONE, 3,
                 "스프링 웹 기술의 핵심을 배운다.", "HTTP 요청과 응답, MVC 구조를 완벽하게 이해함"));
     }
 }
